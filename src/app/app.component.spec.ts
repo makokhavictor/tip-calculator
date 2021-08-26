62c72a43726236c4d4902d1b4696271a899906e4
Makokha Victor <marcosvictos@gmail.com>
2021-08-26 10:28:53 +0300

    calculate tip person and total bill person

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -32,4 +32,28 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('tipcalculator app is running!');
   });
+
+  it('calculateTipPerPerson: should calculate the correct tip per person from bill',()=>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app  = fixture.componentInstance;
+    const billAmount = 142.55;
+    const percentage = 0.15;
+    const numberOfPeople = 5;
+
+    const tipPerPerson = app.calculateTipPerPerson({billAmount,percentage,numberOfPeople});
+    expect(tipPerPerson).toBe(4.28);
+  });
+
+
+  it('calculateTotalBillPerPerson: should get total bill amount for each person',() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app  = fixture.componentInstance;
+    const tipPerPerson = 4.28;
+    const billAmount = 142.55;
+    const numberOfPeople = 5;
+
+    const totalBillPerPerson = app.calculateTotalBillPerPerson({tipPerPerson,billAmount,numberOfPeople});
+
+    expect(totalBillPerPerson).toBe(32.79);
+  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tipcalculator';
+
+
+  calculateTipPerPerson(params:any):Number{
+    const totalTip = params.percentage * params.billAmount;
+    const tipPerPerson = totalTip / params.numberOfPeople;
+    return parseFloat(tipPerPerson.toFixed(2));
+  }
+
+  calculateTotalBillPerPerson(params:any):Number{
+    const billWithoutTip = params.billAmount / params.numberOfPeople;
+    const totalBillPerPerson = billWithoutTip + params.tipPerPerson;
+    return totalBillPerPerson;
+  }
 }
